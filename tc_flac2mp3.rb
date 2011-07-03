c710b1af79044266b3c6c773cb247ee5178c7d86
Bruno Thomas <bthomas@barreverte.fr>
2011-07-03 17:52:46 +0200

    test unitaire

--- tc_flac2mp3.rb
+++ tc_flac2mp3.rb
@@ -7,8 +7,9 @@ class Flac2mp3Test < Test::Unit::TestCase
 		file = File.new("./tmp.wav", "wb")
 		donnees_wav.each {|hex| file.putc hex.to_i(16)}
 		file.close
+		%x[touch cover.jpg]
 		%x[flac -V --totally-silent -f -T "ARTIST=artist" -T "TRACKNUMBER=1" -T "ALBUM=album" -T "TITLE=titre" -T "GENRE=Electronic" -T "DATE=2008" ./tmp.wav]
-		%x[lame --silent -V2 --vbr-new -q0 --lowpass 19.7 --resample 44100 --ignore-tag-errors --ta "artist" --tn "1" --tl "album" --tt "titre" --tg "Electronic" --ty "2008" ./tmp.wav ./tmp_attendu.mp3]
+		%x[lame --silent -V2 --vbr-new -q0 --lowpass 19.7 --resample 44100 ./tmp.wav ./tmp_attendu.mp3 && eyeD3  -a "artist" -n "1" -A "album" -t "titre" --add-image=cover.jpg:FRONT_COVER: -G "Electronic" -Y "2008" --set-encoding=utf8 ./tmp_attendu.mp3]
 		flac2mp3("tmp.flac")
 	 	
 		assert_equal(File::Stat.new("./tmp_attendu.mp3").size, File::Stat.new("./tmp.mp3").size, "la taille des fichiers doit etre la meme")
@@ -16,17 +17,25 @@ class Flac2mp3Test < Test::Unit::TestCase
 		mp3tags_generes = %x[id3tool ./tmp.mp3].gsub(/Filename:.*\n/,"")
 		assert_equal(mp3tags_attendus,mp3tags_generes, "les tags mp3 doivent etre les memes")
 
-		File.delete("./tmp.wav","./tmp.flac","./tmp_attendu.mp3","./tmp.mp3")
+		File.delete("./tmp.wav","./tmp.flac","./tmp_attendu.mp3","./tmp.mp3", "cover.jpg")
 	end	
+
 	def testLitMetaFlac_uneLigne
 		assert_equal( {"TITRE"=>"titre"} ,litMetaFlac("TITRE=titre"))
 	end
+
 	def testLitMetaFlac_deuxLignes
 		assert_equal( {"TITRE"=>"titre","ALBUM"=>"album"} ,litMetaFlac("TITRE=titre\nALBUM=album"))
 	end
+
+	def testLitMetaFlac_deuxLignesAvecMajusculeMinuscule
+		assert_equal( {"TITRE"=>"titre","ALBUM"=>"album"} ,litMetaFlac("TiTrE=titre\nalbum=album"))
+	end
+
 	def testFlac2mp3_fichierNeTerminantPasParFlac
 		assert_raise(RuntimeError) {flac2mp3("fichier.blah")}
 	end
+
 	def testFlac2mp3_fichierInexistant
 		assert_raise(RuntimeError) {flac2mp3("fichier_inexistant.flac")}
 	end
