ead963437117cdce72ce72e5dfee4034f71b7d87
Bruno Thomas <bthomas@barreverte.fr>
2011-07-07 22:11:31 +0200

    ajout de la construction de l'arborescence cible

--- flac2mp3.rb
+++ flac2mp3.rb
@@ -5,6 +5,7 @@ require 'rubygems'
 
 NOMBRE_PROCESSEURS = `cat /proc/cpuinfo | grep processor | wc -l`.to_i
 POOL_SIZE=NOMBRE_PROCESSEURS
+RACINE_REPO_MP3 = "/home/media/musique/mp3/export"
 
 class Flac2mp3
 
@@ -26,12 +27,17 @@ class Flac2mp3
     }
   end
 
-  def flac2mp3(nom_fichier)
-    raise "l'extension du fichier n'est pas .flac :" + nom_fichier if not nom_fichier =~ /.flac$/
-    raise "fichier inexistant :" + nom_fichier if not File.exist? nom_fichier
-    metaflac = litMetaFlac %x[metaflac  --export-tags-to=- "#{nom_fichier}"]
-    nom_fichier_mp3 = nom_fichier.gsub(/\.flac$/, ".mp3")
-    image=File.dirname(nom_fichier) + "/cover.jpg"
+  def flac2mp3(fichier_flac, racine_export_mp3)
+    raise "l'extension du fichier n'est pas .flac :" + fichier_flac if not fichier_flac =~ /.flac$/
+    raise "fichier inexistant :" + fichier_flac if not File.exist? fichier_flac
+    chemin_flac = File.dirname(fichier_flac)
+    chemin_mp3 = racine_export_mp3 + "/" + chemin_flac
+    if (! File.directory?(chemin_mp3))
+      `mkdir -p #{chemin_mp3}`
+    end
+    metaflac = litMetaFlac %x[metaflac  --export-tags-to=- "#{fichier_flac}"]
+    fichier_mp3 = chemin_mp3 + "/" + File.basename(fichier_flac).gsub(/\.flac$/, ".mp3")
+    image= chemin_flac + "/cover.jpg"
     artiste = metaflac["ARTIST"]
     plage = metaflac["TRACKNUMBER"]
     album = metaflac["ALBUM"]
@@ -41,8 +47,8 @@ class Flac2mp3
     if date.include? "-"
       date = date.split('-')[0]
     end
-    puts "flac -dcs #{nom_fichier} | lame --silent -V2 --vbr-new -q0 --lowpass 19.7 --resample 44100 - #{nom_fichier_mp3} && eyeD3  -a #{artiste} -n #{plage} -A #{album} -t #{titre} --add-image #{image}:FRONT_COVER: -G #{genre} -Y #{date} --set-encoding=utf8 #{nom_fichier_mp3}"
-    %x[flac -dcs "#{nom_fichier}" | lame --silent -V2 --vbr-new -q0 --lowpass 19.7 --resample 44100 - "#{nom_fichier_mp3}" && eyeD3  -a "#{artiste}" -n "#{plage}" -A "#{album}" -t "#{titre}" --add-image "#{image}":FRONT_COVER: -G "#{genre}" -Y "#{date}" --set-encoding=utf8 "#{nom_fichier_mp3}"]
+    puts "flac -dcs #{fichier_flac} | lame --silent -V2 --vbr-new -q0 --lowpass 19.7 --resample 44100 - #{fichier_mp3} && eyeD3  -a #{artiste} -n #{plage} -A #{album} -t #{titre} --add-image #{image}:FRONT_COVER: -G #{genre} -Y #{date} --set-encoding=utf8 #{fichier_mp3}"
+    %x[flac -dcs "#{fichier_flac}" | lame --silent -V2 --vbr-new -q0 --lowpass 19.7 --resample 44100 - "#{fichier_mp3}" && eyeD3  -a "#{artiste}" -n "#{plage}" -A "#{album}" -t "#{titre}" --add-image "#{image}":FRONT_COVER: -G "#{genre}" -Y "#{date}" --set-encoding=utf8 "#{fichier_mp3}"]
 
   end
 
--- flac2mp3_acceptance.rb
+++ flac2mp3_acceptance.rb
@@ -1,9 +1,10 @@
 require "flac2mp3"
 require "test/unit"
-RACINE_REPO_MP3 = "test/mp3"
 
 class Flac2mp3Test < Test::Unit::TestCase
-	def setup 
+  
+	def setup
+    @flac2mp3 = Flac2mp3.new
 		Dir.mkdir("test")
 	end
 
@@ -11,47 +12,51 @@ class Flac2mp3Test < Test::Unit::TestCase
  		`rm -rf test`
 	end
 
-	def testRecette
-		creeFichierFlac("./test/tmp.flac")
-		`lame --silent -V2 --vbr-new -q0 --lowpass 19.7 --resample 44100 ./test/tmp.wav ./test/tmp_attendu.mp3 && eyeD3  -a "artist" -n "1" -A "album" -t "titre" --add-image test/cover.jpg:FRONT_COVER: -G "Electronic" -Y "2008" --set-encoding=utf8 ./test/tmp_attendu.mp3`
+  def test_recette
+    cree_fichier_flac("./test/tmp.flac")
+    `lame --silent -V2 --vbr-new -q0 --lowpass 19.7 --resample 44100 ./test/tmp.wav ./test/tmp_attendu.mp3 && eyeD3  -a "artist" -n "1" -A "album" -t "titre" --add-image test/cover.jpg:FRONT_COVER: -G "Electronic" -Y "2008" --set-encoding=utf8 ./test/tmp_attendu.mp3`
+    
+    @flac2mp3.flac2mp3("./test/tmp.flac","./")
+  
+    assert_equal(File::Stat.new("./test/tmp_attendu.mp3").size, File::Stat.new("./test/tmp.mp3").size, "la taille des fichiers doit etre la meme")
+    mp3tags_attendus = %x[id3tool ./test/tmp_attendu.mp3].gsub(/Filename:.*\n/,"")
+    mp3tags_generes = %x[id3tool ./test/tmp.mp3].gsub(/Filename:.*\n/,"")
+    assert_equal(mp3tags_attendus,mp3tags_generes, "les tags mp3 doivent etre les memes")
+  end
+  
+  def test_trouve_fichiers_flac
+    `mkdir -p test/r1 test/r2/r21 test/r3`
+  	`touch test/r1/f11.flac test/r1/f12.flac test/r2/r21/f21.flac`
+    liste_attendue = ["./test/r1/f11.flac", "./test/r1/f12.flac", "./test/r2/r21/f21.flac"].sort()
+    assert_equal(liste_attendue,  @flac2mp3.trouveFichiers(".flac", "./").sort())
+    assert_equal(liste_attendue,  @flac2mp3.trouveFichiers(".flac", "./test/r1", "./test/r2").sort())
+  end
 
-		flac2mp3("./test/tmp.flac")
-	 	
-		assert_equal(File::Stat.new("./test/tmp_attendu.mp3").size, File::Stat.new("./test/tmp.mp3").size, "la taille des fichiers doit etre la meme")
-		mp3tags_attendus = %x[id3tool ./test/tmp_attendu.mp3].gsub(/Filename:.*\n/,"")
-		mp3tags_generes = %x[id3tool ./test/tmp.mp3].gsub(/Filename:.*\n/,"")
-		assert_equal(mp3tags_attendus,mp3tags_generes, "les tags mp3 doivent etre les memes")
-	end	
-
-	def testTrouveFichiersFlac
+	def test_convert_arborescence
 		`mkdir -p test/r1 test/r2/r21 test/r3`
-		`touch test/r1/f11.flac test/r1/f12.flac test/r2/r21/f21.flac`
-		liste_attendue = ["./test/r1/f11.flac", "./test/r1/f12.flac", "./test/r2/r21/f21.flac"].sort()
-		assert_equal(liste_attendue,  trouveFichiers(".flac", ".").sort())
-		assert_equal(liste_attendue,  trouveFichiers(".flac", "./test/r1", "./test/r2").sort())
-	end
+		cree_fichier_flac("test/r1/f11.flac")
+		cree_fichier_flac("test/r1/f12.flac")
+		cree_fichier_flac("test/r2/r21/f21.flac")
+    begin
+      # il faut etre dans le repertoire racine des flacs
+      Dir.chdir("test")
 
-	def testConvertiArborescence
-		`mkdir -p test/r1 test/r2/r21 test/r3`
-		creeFichierFlac("test/r1/f11.flac")
-		creeFichierFlac("test/r1/f12.flac")
-		creeFichierFlac("test/r2/r21/f21.flac")
-		Dir.mkdir(RACINE_REPO_MP3)
+      @flac2mp3.trouveFichiers(".flac", "./").each {|flac| @flac2mp3.flac2mp3(flac, "mp3")}
 
-		flac2mp3("test/")
+      assert_equal ["./mp3/r1/f11.mp3", "./mp3/r1/f12.mp3", "./mp3/r2/r21/f21.mp3"].sort, @flac2mp3.trouveFichiers(".mp3", "./").sort
+    ensure Dir.chdir("..")
+    end
+  end
 
-		assert_equal ["test/mp3/r1/f11.mp3", "test/mp3/r1/f12.mp3", "test/mp3/r2/r21/f21.mp3"].sort, trouveFichiers(".mp3", "test").sort
-	end
-
-	def creeFichierFlac(nom)
-		if !File.exist?("./test/tmp.wav")
-			donnees_wav=["52","49","46","46","24","08","00","00","57","41","56","45","66","6d","74","20","10","00","00","00","01","00","02","00","22","56","00","00","88","58","01","00","04","00","10","00","64","61","74","61","00","08","00","00","00","00","00","00","24","17","1e","f3","3c","13","3c","14","16","f9","18","f9","34","e7","23","a6","3c","f2","24","f2","11","ce","1a","0d"]
-			file = File.new("./test/tmp.wav", "wb")
-			donnees_wav.each {|hex| file.putc hex.to_i(16)}
-			file.close
-		end
-		image = File.dirname(nom) + "/cover.jpg"
-		`touch #{image}`
-		`flac -V --totally-silent -f -T "ARTIST=artist" -T "TRACKNUMBER=1" -T "ALBUM=album" -T "TITLE=titre" -T "GENRE=Electronic" -T "DATE=2008" ./test/tmp.wav -o ./test/tmp.flac`
-	end
+  def cree_fichier_flac(nom)
+    if !File.exist?("./test/tmp.wav")
+      donnees_wav=["52","49","46","46","24","08","00","00","57","41","56","45","66","6d","74","20","10","00","00","00","01","00","02","00","22","56","00","00","88","58","01","00","04","00","10","00","64","61","74","61","00","08","00","00","00","00","00","00","24","17","1e","f3","3c","13","3c","14","16","f9","18","f9","34","e7","23","a6","3c","f2","24","f2","11","ce","1a","0d"]
+      file = File.new("./test/tmp.wav", "wb")
+      donnees_wav.each {|hex| file.putc hex.to_i(16)}
+      file.close
+    end
+    image = File.dirname(nom) + "/cover.jpg"
+    `touch #{image}`
+    `flac -V --totally-silent -f -T "ARTIST=artist" -T "TRACKNUMBER=1" -T "ALBUM=album" -T "TITLE=titre" -T "GENRE=Electronic" -T "DATE=2008" ./test/tmp.wav -o #{nom}`
+  end
 end
