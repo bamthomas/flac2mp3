fdcea5a6a3eb7a9ca289fa390e69d4c1878cd3fe
Bruno Thomas <bthomas@barreverte.fr>
2011-07-07 13:56:28 +0200

    refactor creation fichier flac

--- flac2mp3_acceptance.rb
+++ flac2mp3_acceptance.rb
@@ -11,12 +11,7 @@ class Flac2mp3Test < Test::Unit::TestCase
 	end
 
 	def testRecette
-		donnees_wav=["52","49","46","46","24","08","00","00","57","41","56","45","66","6d","74","20","10","00","00","00","01","00","02","00","22","56","00","00","88","58","01","00","04","00","10","00","64","61","74","61","00","08","00","00","00","00","00","00","24","17","1e","f3","3c","13","3c","14","16","f9","18","f9","34","e7","23","a6","3c","f2","24","f2","11","ce","1a","0d"]
-		file = File.new("./test/tmp.wav", "wb")
-		donnees_wav.each {|hex| file.putc hex.to_i(16)}
-		file.close
-		`touch test/cover.jpg`
-		`flac -V --totally-silent -f -T "ARTIST=artist" -T "TRACKNUMBER=1" -T "ALBUM=album" -T "TITLE=titre" -T "GENRE=Electronic" -T "DATE=2008" ./test/tmp.wav -o ./test/tmp.flac`
+		creeFichierFlac("./test/tmp.flac")
 		`lame --silent -V2 --vbr-new -q0 --lowpass 19.7 --resample 44100 ./test/tmp.wav ./test/tmp_attendu.mp3 && eyeD3  -a "artist" -n "1" -A "album" -t "titre" --add-image test/cover.jpg:FRONT_COVER: -G "Electronic" -Y "2008" --set-encoding=utf8 ./test/tmp_attendu.mp3`
 
 		flac2mp3("./test/tmp.flac")
@@ -33,5 +28,21 @@ class Flac2mp3Test < Test::Unit::TestCase
 		liste_attendue = ["./test/r1/f11.flac", "./test/r1/f12.flac", "./test/r2/r21/f21.flac"].sort()
 		assert_equal(liste_attendue,  trouveFlac(".").sort())
 		assert_equal(liste_attendue,  trouveFlac("./test/r1", "./test/r2").sort())
+	end
+
+	def convertiArborescence
+		
+	end
+
+	def creeFichierFlac(nom)
+		if !File.exist?("./test/tmp.wav")
+			donnees_wav=["52","49","46","46","24","08","00","00","57","41","56","45","66","6d","74","20","10","00","00","00","01","00","02","00","22","56","00","00","88","58","01","00","04","00","10","00","64","61","74","61","00","08","00","00","00","00","00","00","24","17","1e","f3","3c","13","3c","14","16","f9","18","f9","34","e7","23","a6","3c","f2","24","f2","11","ce","1a","0d"]
+			file = File.new("./test/tmp.wav", "wb")
+			donnees_wav.each {|hex| file.putc hex.to_i(16)}
+			file.close
+		end
+		image = File.dirname(nom) + "/cover.jpg"
+		`touch #{image}`
+		`flac -V --totally-silent -f -T "ARTIST=artist" -T "TRACKNUMBER=1" -T "ALBUM=album" -T "TITLE=titre" -T "GENRE=Electronic" -T "DATE=2008" ./test/tmp.wav -o ./test/tmp.flac`
 	end
 end
