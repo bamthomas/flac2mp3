a7163a1f7d90f6b5691dd8f5c22fbc1a2d34d00d
Bruno Thomas <bthomas@barreverte.fr>
2011-07-11 20:20:55 +0200

    reduction de la taille des tabulations

--- lib/convert_id3v2_to_utf8.py
+++ lib/convert_id3v2_to_utf8.py
@@ -8,26 +8,26 @@ from eyeD3.frames import *;
 tag = eyeD3.Tag()
 
 class TextFrameWithUtf8Encoding(Frame):
-	def __init__(self, textFrame):
-		Frame.__init__(self, textFrame.header, textFrame.unsync_default);
-		self.textFrame = textFrame;
+  def __init__(self, textFrame):
+    Frame.__init__(self, textFrame.header, textFrame.unsync_default);
+    self.textFrame = textFrame;
 
-	def render(self):
-		if self.textFrame.header.minorVersion == 4 and self.textFrame.header.id == "TSIZ":
-			TRACE_MSG("Dropping deprecated frame TSIZ")
-			return ""
-		data = UTF_8_ENCODING + self.textFrame.text.encode(id3EncodingToString(self.textFrame.encoding));
-		return self.textFrame.assembleFrame(data);
+  def render(self):
+    if self.textFrame.header.minorVersion == 4 and self.textFrame.header.id == "TSIZ":
+      TRACE_MSG("Dropping deprecated frame TSIZ")
+      return ""
+    data = UTF_8_ENCODING + self.textFrame.text.encode(id3EncodingToString(self.textFrame.encoding));
+    return self.textFrame.assembleFrame(data);
 
 if len(sys.argv) != 2:
-	print "usage : " + sys.argv[0] + " <mp3_file>";
-	sys.exit(1);
+  print "usage : " + sys.argv[0] + " <mp3_file>";
+  sys.exit(1);
 
 tag.link(sys.argv[1])
 
 for i in range(0, len(tag.frames)):
-	frame=tag.frames[i];
-	if frame.__class__ == TextFrame:
-		tag.frames[i] = TextFrameWithUtf8Encoding(frame);
+  frame=tag.frames[i];
+  if frame.__class__ == TextFrame:
+    tag.frames[i] = TextFrameWithUtf8Encoding(frame);
 
 tag.update()
