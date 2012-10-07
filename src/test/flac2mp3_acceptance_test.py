efb6ff8a1f381068c6a2e51b0f58c4ae67668b43
Bruno Thomas <bthomas@barreverte.fr>
2012-10-07 23:30:24 +0200

    added python acceptance test

--- src/test/flac2mp3_acceptance_test.py
+++ src/test/flac2mp3_acceptance_test.py
@@ -1,7 +1,35 @@
+# -*- coding: UTF-8 -*-
+import subprocess
+from os.path import dirname, getsize
 import unittest
+import binascii
+import eyeD3
+from flac2mp3_test import Flac2Mp3, LAME_COMMAND
 
 __author__ = 'bruno thomas'
 
 
 class TestFlac2Mp3Acceptance(unittest.TestCase):
-    pass
+    def test_recette(self):
+        self.cree_fichier_flac('/tmp/tmp.flac')
+
+        Flac2Mp3().run('/tmp/tmp.flac','/tmp')
+
+        tag = eyeD3.Tag()
+        tag.link('/tmp/tmp.mp3')
+        self.assertEquals(u"artist", tag.getArtist())
+        self.assertEquals((1,15), tag.getTrackNum())
+        self.assertEquals(u"album", tag.getAlbum())
+        self.assertEquals(u"title", tag.getTitle())
+        self.assertEquals('Electronic', tag.getGenre().getName())
+        self.assertEquals('2008', (tag.getDate()[0]).getYear())
+
+
+    def cree_fichier_flac(self, flac_file):
+        with open('/tmp/tmp.wav', 'wb') as mp3:
+            mp3.write(binascii.a2b_hex("524946462408000057415645666d7420100000000100020022560000885801000400100064617461000800000000000024171ef33c133c1416f918f934e723a63cf224f211ce1a0d"))
+
+        open(dirname(flac_file) + "/cover.jpg", 'w').close()
+
+        flac_cmde = u'/usr/bin/flac -V --totally-silent -f -T ARTIST=artist -T TRACKNUMBER=1 -T TRACKTOTAL=15 -T ALBUM=album -T TITLE=title -T GENRE=Electronic -T DATE=2008 /tmp/tmp.wav -o %s' % flac_file
+        subprocess.call(flac_cmde.split(' '))
--- src/test/flac2mp3_test.py
+++ src/test/flac2mp3_test.py
@@ -1,15 +1,34 @@
 # -*- coding: UTF-8 -*-
 import commands
+from os.path import dirname
+from subprocess import call
 import unittest
+import eyeD3
+from eyeD3.frames import ImageFrame
+
+LAME_COMMAND = 'flac -dcs %s | lame --silent -V2 --vbr-new -q0 --lowpass 19.7 --resample 44100 --add-id3v2 - %s'
 
 __author__ = 'bruno thomas'
 
 class Flac2Mp3(object):
     def lit_meta_flac(self, tags):
         return dict((line.split('=')[0].upper(), line.split('=')[1].replace('\r','\r\n')) for line in tags.replace('\r\n','\r').split('\n'))
-    
+
     def run(self, flac_file, export_path):
         tags=self.lit_meta_flac(commands.getoutput('metaflac  --export-tags-to=- %s' % flac_file))
+        mp3_file = flac_file.replace('.flac', '.mp3')
+        call(LAME_COMMAND % (flac_file, mp3_file),shell=True)
+        tag = eyeD3.Tag(mp3_file)
+        tag.link(mp3_file)
+        tag.setArtist(tags['ARTIST'])
+        tag.setTrackNum([tags['TRACKNUMBER'],tags['TRACKTOTAL']])
+        tag.setAlbum(tags['ALBUM'])
+        tag.setTitle(tags['TITLE'])
+        tag.setGenre(tags['GENRE'])
+        tag.setDate(tags['DATE'])
+        tag.addImage(ImageFrame.FRONT_COVER, dirname(flac_file) + "/cover.jpg")
+        tag.update()
+
 
 class TestFlac2Mp3(unittest.TestCase):
     def test_lit_metaflac_une_ligne(self):
