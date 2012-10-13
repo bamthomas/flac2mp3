e757c12111c7c57a48600c15a78a7a2ba38d1818
Bruno Thomas <bthomas@barreverte.fr>
2012-10-13 15:12:40 +0200

    [fix] concurrent directory creation bug fixed

--- src/main/flac2mp3.py
+++ src/main/flac2mp3.py
@@ -20,20 +20,21 @@ __author__ = 'bruno thomas'
 
 LAME_COMMAND = 'flac -dcs %s | lame --silent -V2 --vbr-new -q0 --lowpass 19.7 --resample 44100 --add-id3v2 - %s'
 POOL_SIZE = int(check_output('cat /proc/cpuinfo | grep processor | wc -l', shell=True))
-logging.basicConfig(format='%(asctime)s [%(name)s] %(levelname)s: %(message)s' )
-LOGGER=logging.getLogger('flac2mp3')
+logging.basicConfig(format='%(asctime)s [%(name)s] %(levelname)s: %(message)s')
+LOGGER = logging.getLogger('flac2mp3')
 
 def lit_meta_flac(tags):
-    return dict((line.split('=')[0].upper(), line.split('=')[1].replace('\r','\r\n')) for line in tags.replace('\r\n','\r').split('\n'))
+    return dict((line.split('=')[0].upper(), line.split('=')[1].replace('\r', '\r\n')) for line in tags.replace('\r\n', '\r').split('\n'))
+
 
 def transcode(flac_file, mp3_file):
-    tags=lit_meta_flac(commands.getoutput('metaflac  --export-tags-to=- %s' % flac_file))
-    LOGGER.info('transcoding %s with tags (title=%s artist=%s track=%s/%s)', flac_file, tags['TITLE'], tags['ARTIST'], tags['TRACKNUMBER'],tags['TRACKTOTAL'])
-    call(LAME_COMMAND % (flac_file, mp3_file),shell=True)
+    tags = lit_meta_flac(commands.getoutput('metaflac  --export-tags-to=- %s' % flac_file))
+    LOGGER.info('transcoding %s with tags (title=%s artist=%s track=%s/%s)', flac_file, tags['TITLE'], tags['ARTIST'], tags['TRACKNUMBER'], tags['TRACKTOTAL'])
+    call(LAME_COMMAND % (flac_file, mp3_file), shell=True)
     tag = eyeD3.Tag(mp3_file)
     tag.link(mp3_file)
     tag.setArtist(tags['ARTIST'])
-    tag.setTrackNum([tags['TRACKNUMBER'],tags['TRACKTOTAL']])
+    tag.setTrackNum([tags['TRACKNUMBER'], tags['TRACKTOTAL']])
     tag.setAlbum(tags['ALBUM'])
     tag.setTitle(tags['TITLE'])
     tag.setGenre(tags['GENRE'])
@@ -41,39 +42,50 @@ def transcode(flac_file, mp3_file):
     tag.addImage(ImageFrame.FRONT_COVER, dirname(flac_file) + "/cover.jpg")
     tag.update()
 
+
 def trouve_fichiers(extension, *repertoires_racines):
     for repertoire_racine in repertoires_racines:
         for root, _, files in os.walk(repertoire_racine):
             for file in files:
                 if file.endswith(extension): yield join(root, file)
 
+
 def get_mp3_file(mp3_target_path, flac_root_path, flac_file):
     flac_path_relative_to_root = flac_file.replace(flac_root_path, '').replace('.flac', '.mp3')
     if flac_path_relative_to_root.startswith('/'): flac_path_relative_to_root = flac_path_relative_to_root[1:]
     return join(mp3_target_path, flac_path_relative_to_root)
 
+
 def process_transcoding(flac_file, flac_root_path, mp3_target_path):
+    try:
         target_mp3_file = get_mp3_file(mp3_target_path, flac_root_path, flac_file)
         if not isdir(dirname(target_mp3_file)):
-            os.makedirs(dirname(target_mp3_file))
+            try:
+                os.makedirs(dirname(target_mp3_file))
+            except OSError:
+                pass # other thread might have been faster
         transcode(flac_file, target_mp3_file)
+    except Exception as e:
+        LOGGER.error('error during the transcoding of %s : %s' % (flac_file, e))
+
 
 def run(mp3_target_path, flac_root_path, *flac_path_list):
     flac_files = set(trouve_fichiers('.flac', *flac_path_list))
     LOGGER.info('found %d flac files', len(flac_files))
-    LOGGER.info('transcoding files with command "%s"', LAME_COMMAND % ('file.flac','file.mp3'))
+    LOGGER.info('transcoding files with command "%s"', LAME_COMMAND % ('file.flac', 'file.mp3'))
 
     thread_pool = Pool(POOL_SIZE)
-
     for flac_file in flac_files:
         thread_pool.apply_async(process_transcoding, args=(flac_file, flac_root_path, mp3_target_path))
     thread_pool.close()
     thread_pool.join()
 
+
 class Usage(Exception):
     def __init__(self, msg, *args, **kwargs):
         super(Usage, self).__init__(*args, **kwargs)
         self.msg = msg
+
 
 def main(argv):
     try:
@@ -96,8 +108,8 @@ def main(argv):
 
         run(mp3_target_path, getcwd(), *args)
     except Usage, err:
-        print >>sys.stderr, err.msg
-        print >>sys.stderr, "for help use --help"
+        print >> sys.stderr, err.msg
+        print >> sys.stderr, "for help use --help"
         return 2
 
 if __name__ == "__main__":
