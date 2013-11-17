4c2a2dc31d8adb665ecfe845f142a43c603e2577
Bruno Thomas <bruno@barreverte.fr>
2013-11-17 21:37:49 +0100

    fix invalid syntax for with A, B -> with A with B

--- test/flac2mp3_acceptance_test.py
+++ test/flac2mp3_acceptance_test.py
@@ -46,15 +46,16 @@ class TestFlac2Mp3Acceptance(unittest.TestCase):
             self.assertEquals(1, transcode.count())
 
     def test_target_mp3_exists_with_differents_tags_flac_is_transcoded_again(self):
-        with TemporaryDirectory() as tmp, CountingTranscodeCalls() as transcode:
-            flac_file, mp3_file = self.init_files(tmp)
+        with TemporaryDirectory() as tmp:
+            with CountingTranscodeCalls() as transcode:
+                flac_file, mp3_file = self.init_files(tmp)
 
-            flac2mp3.process_transcoding((flac_file, tmp, tmp))
+                flac2mp3.process_transcoding((flac_file, tmp, tmp))
 
-            self.create_flac_file(flac_file, tags={'ARTIST': u'artist'})
-            flac2mp3.process_transcoding((flac_file, tmp, tmp))
+                self.create_flac_file(flac_file, tags={'ARTIST': u'artist'})
+                flac2mp3.process_transcoding((flac_file, tmp, tmp))
 
-            self.assertEquals(2, transcode.count())
+                self.assertEquals(2, transcode.count())
 
     def test_acceptance_one_file_with_embedded_cover(self):
         with TemporaryDirectory() as tmp:
