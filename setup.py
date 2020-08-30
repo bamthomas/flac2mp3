e71094c40c7bab1328180af8fec38a485753a90d
Bruno Thomas <bruno@barreverte.fr>
2020-08-30 18:26:11 +0200

    [doc] update compatibility in setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(
     long_description=README,
     classifiers=[
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
